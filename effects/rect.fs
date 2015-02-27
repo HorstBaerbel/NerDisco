c9fc46b9d6a4410beca388d09275454bf751e60c
Bim <bim.overbohm@googlemail.com>
2015-02-27 01:01:09 +0100

    Update README regarding (Rt)MIDI

--- effects/rect.fs
+++ effects/rect.fs
@@ -12,8 +12,8 @@ varying vec2 texcoordVar;
 void main() {
 	float l = texcoordVar.x > 0.2+0.1*sin(time) ? (texcoordVar.x < 0.8+0.2*sin(0.88*time+1) ? 1 : 0) : 0;
 	l = texcoordVar.y > 0.2+0.1*sin(0.69*time) ? (texcoordVar.y < 0.8+0.2*sin(0.45*time) ? l : 0) : 0;
-	float r = l * valueA + trigger;
-	float g = l * valueB + trigger;
-	float b = l * valueC + trigger;
+	float r = l * valueA + 0.1*trigger;
+	float g = l * valueB + 0.1*trigger;
+	float b = l * valueC + 0.1*trigger;
 	gl_FragColor = vec4(r, g, b, 1.0);
 }
